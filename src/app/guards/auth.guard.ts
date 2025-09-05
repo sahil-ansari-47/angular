bc63f624455749e7be1129a072431719260ccc9c
sahil-ansari-47 <sahilansari28112003@gmail.com>
2025-09-06 04:03:33 +0530

    auth guard fix

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,22 +2,25 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { environment } from '../../environments/environment';
 
-export const authGuard: CanActivateFn = async() => {
+export const authGuard: CanActivateFn = async () => {
   const router = inject(Router);
-
   const apiUrl = environment.apiUrl;
 
   try {
-    const res = await fetch(`${apiUrl}/api/auth/me`, {headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }}).then((res) => {
-      if (!res.ok) return null; // don’t parse if unauthorized
-      return res.json();
-    })
+    const response = await fetch(`${apiUrl}/api/auth/me`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
 
-    // Parse JSON
-    const user = await res.json();
+    if (!response.ok) {
+      console.log('authGuard: unauthorized');
+      router.navigate(['/']);
+      return false;
+    }
 
-    if (user) {
-      console.log('authGuard user');
+    const user = await response.json();
+
+    if (user && !user.error) {
+      console.log('authGuard user', user);
       return true;
     } else {
       console.log('authGuard no user');
