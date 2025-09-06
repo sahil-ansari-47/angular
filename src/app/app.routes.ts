8a94530d7d80b348592c66c119163e80cd61d8a1
sahil-ansari-47 <sahilansari28112003@gmail.com>
2025-09-07 02:08:24 +0530

    rm callback lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,25 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard'; // Assuming you have an authGuard.ts file
+import { AuthCallback } from './auth-callback/auth-callback';
 
 export const routes: Routes = [
   // Lazy-loaded route for the home page.
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: () => import('./home/home').then(m => m.Home),
+    loadComponent: () => import('./home/home').then((m) => m.Home),
   },
   // Protected, lazy-loaded route for the dashboard.
   {
     path: 'dashboard',
     canActivate: [authGuard],
-    loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard),
+    loadComponent: () =>
+      import('./dashboard/dashboard').then((m) => m.Dashboard),
   },
   //  Lazy-loaded route for the auth callback page.
   {
     path: 'auth/callback',
-    loadComponent: () => import('./auth-callback/auth-callback').then(m => m.AuthCallback),
+    component: AuthCallback,
   },
   // Wildcard route to redirect to the home page for any unknown URLs.
   {
